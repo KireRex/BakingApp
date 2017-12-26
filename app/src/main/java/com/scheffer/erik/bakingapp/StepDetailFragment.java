10c89f2e87878ca3e0c9da4cb6d719eff0ce7710
esscheffer <erikscheffer@yahoo.com>
2017-12-26 20:17:25 -0200

    Added video to step details

--- app/src/main/java/com/scheffer/erik/bakingapp/StepDetailFragment.java
+++ app/src/main/java/com/scheffer/erik/bakingapp/StepDetailFragment.java
@@ -1,6 +1,7 @@
 package com.scheffer.erik.bakingapp;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
@@ -9,12 +10,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.scheffer.erik.bakingapp.models.Step;
 
 public class StepDetailFragment extends Fragment {
     public static final String STEP_EXTRA_KEY = "step";
 
     private Step step;
+    private SimpleExoPlayer exoPlayer;
+    private SimpleExoPlayerView playerView;
 
     public StepDetailFragment() {
     }
@@ -35,8 +50,43 @@ public class StepDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.step_detail, container, false);
 
-        ((TextView) rootView.findViewById(R.id.step_detail)).setText(step.getDescription());
+        ((TextView) rootView.findViewById(R.id.step_detail_text)).setText(step.getDescription());
+        playerView = rootView.findViewById(R.id.step_video);
 
+        loadVideo();
         return rootView;
+    }
+
+    private void loadVideo() {
+        if (exoPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this.getActivity()),
+                                                           trackSelector,
+                                                           loadControl);
+            playerView.setPlayer(exoPlayer);
+            exoPlayer.prepare(
+                    new ExtractorMediaSource(Uri.parse(step.getVideoURL()),
+                                             new DefaultDataSourceFactory(
+                                                     this.getActivity(),
+                                                     Util.getUserAgent(this.getActivity(),
+                                                                       "BakingApp")),
+                                             new DefaultExtractorsFactory(),
+                                             null,
+                                             null));
+            exoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        exoPlayer.stop();
+        exoPlayer.release();
+        exoPlayer = null;
     }
 }
