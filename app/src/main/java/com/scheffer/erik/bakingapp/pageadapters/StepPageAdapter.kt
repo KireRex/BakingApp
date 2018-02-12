2a59c33159f84b338c76e6779762dad01dbc0891
esscheffer <erikscheffer@yahoo.com>
2018-02-12 10:34:30 -0200

    Converted PageAdapters

---
+++ app/src/main/java/com/scheffer/erik/bakingapp/pageadapters/StepPageAdapter.kt
@@ -0,0 +1,27 @@
+package com.scheffer.erik.bakingapp.pageadapters
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import com.scheffer.erik.bakingapp.R
+import com.scheffer.erik.bakingapp.fragments.StepDetailFragment
+import com.scheffer.erik.bakingapp.models.Step
+
+
+class StepPageAdapter(fm: FragmentManager,
+                      private val steps: List<Step>,
+                      private val context: Context) : FragmentStatePagerAdapter(fm) {
+
+    override fun getItem(position: Int) =
+            StepDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(StepDetailFragment.STEP_EXTRA_KEY, steps[position])
+                }
+            }
+
+    override fun getCount() = steps.size
+
+    override fun getPageTitle(position: Int): CharSequence = context.resources.getString(R.string.step_tab_title,
+                                                                                         position)
+}
